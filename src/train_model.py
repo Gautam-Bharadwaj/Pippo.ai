e4d9f0622f91f7c0cb82ea34f01a535ef258de38
JHAJI01 <kr.gautamjha03@gmail.com>
2026-03-19 00:43:43 +0530

    chore(ml): update training parameters for improved risk classification

--- src/train_model.py
+++ src/train_model.py
@@ -20,7 +20,7 @@ def train_and_save_model():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
     model = Pipeline([
-        ('vectorizer', TfidfVectorizer(max_features=5000, stop_words='english')),
+        ('vectorizer', TfidfVectorizer(max_features=2000, stop_words='english')),
         ('classifier', RandomForestClassifier(n_estimators=100, random_state=42, n_jobs=-1))
     ])
     
